72e2633ba45ec7c68357cc5830475e1023295d10
apurbasarkarr <apurbasarkar651@gmail.com>
2025-03-18 12:09:17 +0530

    add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+
+import {Route, Routes } from "react-router-dom";
+import Start from "./pages/Start.jsx";
+import Home from "./pages/Home.jsx";
+import UserLogin from "./pages/UserLogin.jsx";
+import UserSignUp from "./pages/UserSignUp";
+
 const App = () => {
-  return <div className="bg-blue-500">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Start/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/login' element={<UserLogin/>} />
+        <Route path='/signup' element={<UserSignUp/>} />
+
+      </Routes>
+    </div>
+  )
 };
 
 export default App;
